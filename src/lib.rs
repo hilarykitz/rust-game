4fdd0591e122a845c90cc656cae855969918f309
Stef <stef.jones@condenast.com>
2020-02-07 21:18:47 +0000

    Add eat book feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,36 @@
 pub mod game {
     # [derive(Debug)]
+    pub enum Entity {
+        Book,
+    }
+
+    # [derive(Debug)]
     pub enum Instruction {
         Look,
+        Eat(Entity),
     }
 
     pub fn parse_instruction(instruction: &str) -> Result<Instruction, &str> {
         let instruction = instruction.trim();
         let tokens: Vec<&str> = instruction.split(" ").collect();
 
-        if tokens.len() > 0 && tokens[0] == "look" {
-            if tokens.len() > 1 {
-                return Err("I can't find that")
-            }
+        if tokens.len() > 0 {
+            if tokens[0] == "look" {
+                if tokens.len() > 1 {
+                    return Err("I can't find that")
+                }
 
-            return Ok(Instruction::Look)
+                return Ok(Instruction::Look)
+            } else if tokens[0] == "eat" {
+                if tokens.len() > 1 {
+                    if tokens[1] == "book" {
+                        return Ok(Instruction::Eat(Entity::Book));
+                    }
+                    return Err("I can't find that")
+                }
+
+                return Err("You don't have anything to eat");
+            }
         }
 
         Err("I don't understand")
