2689f7cea0036f642c9bd5740247bfd935a58d56
Stef <stef.jones@condenast.com>
2020-02-09 17:54:28 +0000

    Add scene type

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-pub mod game {
+mod parser {
     #[derive(Debug,PartialEq)]
     pub enum EntityIdent {
         Book,
@@ -16,27 +16,13 @@ pub mod game {
     }
 
     #[derive(Debug,PartialEq)]
-    pub struct Entity {
-        ident: EntityIdent,
-    }
-
-    impl Entity {
-        pub fn from_str(string: &str) -> Option<Entity> {
-            match EntityIdent::from_str(string) {
-                Some(ident) => Some(Entity { ident }),
-                None => None,
-            }
-        }
-    }
-
-    #[derive(Debug,PartialEq)]
     pub enum Instruction {
         Look,
         Describe(Option<EntityIdent>),
         Consume(Option<EntityIdent>),
     }
 
-    pub fn parse_instruction(instruction: &str) -> Result<Instruction, &str> {
+    pub fn parse_instruction(instruction: String) -> Result<Instruction, &'static str> {
         let instruction = instruction.trim();
         let tokens: Vec<&str> = instruction.split(" ").collect();
 
@@ -57,67 +43,162 @@ pub mod game {
 
         Err("I don't understand")
     }
+}
 
-    pub fn get_instruction_response(instruction: &Instruction) -> &str {
-        match instruction {
-            Instruction::Look => "You look around and see a book and a sandwich",
-            Instruction::Describe(Some(entity)) => match entity {
-                EntityIdent::Book => "It's a book",
-                EntityIdent::Sandwich => "It's a sandwich",
-            },
-            Instruction::Describe(None) => "You can't see it",
-            Instruction::Consume(Some(entity)) => match entity {
-                EntityIdent::Sandwich => "The sandwich tastes great and you eat the whole thing",
-                EntityIdent::Book => "The book tastes like sweeties and you absorb the knowledge within",
-            },
-            Instruction::Consume(None) => "You can't find that"
+pub mod game {
+    use super::parser;
+
+    trait Entity {
+        fn describe(&self) -> String;
+
+        fn consume(&self) -> Result<String, &str> {
+            Err("It's not food")
+        }
+
+        fn read(&self) -> Result<String, &str> {
+            Err("There's nothing to read")
+        }
+    }
+
+    struct Apple {
+        eaten: bool
+    }
+
+    impl Apple {
+        fn new() -> Apple {
+            Apple {
+                eaten: false
+            }
+        }
+    }
+
+    impl Entity for Apple {
+        fn describe(&self) -> String {
+            if !self.eaten {
+                String::from("It's a perfect red apple")
+            } else {
+                String::from("It's an apple core")
+            }
+        }
+
+        fn consume(&self) -> Result<String, &str> {
+            if !self.eaten {
+                Ok(String::from("It's delicious! All that's left is the core"))
+            } else {
+                Err("The core doesn't look appetising")
+            }
+        }
+    }
+
+    struct Book {
+        title: String,
+        author: String,
+        contents: String,
+    }
+
+    impl Book {
+        fn new(title: String, author: String, contents: String) -> Book {
+            Book {
+                title,
+                author,
+                contents,
+            }
+        }
+    }
+
+    impl Entity for Book {
+        fn describe(&self) -> String {
+            format!("It's a book. The title reads \"{}\" by {}", &self.title, &self.author)
+        }
+
+        fn read(&self) -> Result<String, &str> {
+            Ok(format!("The book reads:\n{}", &self.contents))
+        }
+    }
+
+    pub struct Scene {
+        entities: Vec<Box<dyn Entity>>
+    }
+
+    impl Scene {
+        pub fn new() -> Scene {
+            Scene {
+                entities: vec![
+                    Box::new(Apple::new()),
+                    Box::new(Book::new(String::from("The Lusty Argonian Maid"), String::from("Crassius Curio"), String::from("[contents here]"))),
+                ]
+            }
+        }
+
+        pub fn do_instruction(&mut self, instruction: String) {
+            match parser::parse_instruction(instruction) {
+                Ok(instruction) => {
+                    let response = match instruction {
+                        parser::Instruction::Look => "You look around and see a book and a sandwich",
+                        parser::Instruction::Describe(Some(entity)) => match entity {
+                            parser::EntityIdent::Book => "It's a book",
+                            parser::EntityIdent::Sandwich => "It's a sandwich",
+                        },
+                        parser::Instruction::Describe(None) => "You can't see it",
+                        parser::Instruction::Consume(Some(entity)) => match entity {
+                            parser::EntityIdent::Sandwich => "The sandwich tastes great and you eat the whole thing",
+                            parser::EntityIdent::Book => "The book tastes like sweeties and you absorb the knowledge within",
+                        },
+                        parser::Instruction::Consume(None) => "You can't find that"
+                    };
+                    println!("{}\n", response);
+                },
+                Err(error) => {
+                    println!("{}\n", error);
+                }
+            }
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::game::*;
+    use super::parser::*;
 
     #[test]
     fn it_parses_bad_input() {
-        assert_eq!(parse_instruction(""), Err("I don't understand"));
-        assert_eq!(parse_instruction("dance"), Err("I don't understand"));
+        assert_eq!(parse_instruction(String::from("")), Err("I don't understand"));
+        assert_eq!(parse_instruction(String::from("dance")), Err("I don't understand"));
     }
 
     #[test]
     fn it_parses_look() {
-        let instruction = parse_instruction("look").unwrap();
+        let instruction = parse_instruction(String::from("look")).unwrap();
         assert_eq!(instruction, Instruction::Look);
     }
 
     #[test]
     fn it_parses_look_at() {
-        let instruction = parse_instruction("look at");
+        let instruction = parse_instruction(String::from("look at"));
         assert_eq!(instruction, Err("I don't understand"));
 
-        let instruction = parse_instruction("look at book").unwrap();
+        let instruction = parse_instruction(String::from("look at book")).unwrap();
         assert_eq!(instruction, Instruction::Describe(EntityIdent::from_str("book")));
 
-        let instruction = parse_instruction("look at sandwich").unwrap();
+        let instruction = parse_instruction(String::from("look at sandwich")).unwrap();
         assert_eq!(instruction, Instruction::Describe(EntityIdent::from_str("sandwich")));
 
-        let instruction = parse_instruction("look at dolphin").unwrap();
+        let instruction = parse_instruction(String::from("look at dolphin")).unwrap();
         assert_eq!(instruction, Instruction::Describe(None));
     }
 
     #[test]
     fn it_parses_eat() {
-        let instruction = parse_instruction("eat");
+        let instruction = parse_instruction(String::from("eat"));
         assert_eq!(instruction, Err("I don't understand"));
 
-        let instruction = parse_instruction("eat book").unwrap();
+        let instruction = parse_instruction(String::from("eat book")).unwrap();
         assert_eq!(instruction, Instruction::Consume(EntityIdent::from_str("book")));
 
-        let instruction = parse_instruction("eat sandwich").unwrap();
+        let instruction = parse_instruction(String::from("eat sandwich")).unwrap();
         assert_eq!(instruction, Instruction::Consume(EntityIdent::from_str("sandwich")));
 
-        let instruction = parse_instruction("eat dolphin").unwrap();
+        let instruction = parse_instruction(String::from("eat dolphin")).unwrap();
         assert_eq!(instruction, Instruction::Consume(None));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,15 @@ use std::io;
 use rust_game::game;
 
 fn main() {
+    let mut scene = game::Scene::new();
+
     loop {
         let mut instruction = String::new();
 
         if let Err(error) = io::stdin().read_line(&mut instruction) {
             dbg!(error);
         } else {
-            match game::parse_instruction(&instruction[..]) {
-                Ok(instruction) => {
-                    let response = game::get_instruction_response(&instruction);
-                    println!("{}\n", response);
-                },
-                Err(error) => {
-                    println!("{}\n", error);
-                }
-            }
+            scene.do_instruction(instruction);
         }
     }
 }
